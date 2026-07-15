d23186e839ea62c78fd32eb9219333fc6e0e26ee
Vishwajeet Patil <vishwajeetpatil4951@gmail.com>
2026-07-15 22:31:58 +0530

    Time: 3 ms (18.29%), Space: 19.5 MB (9.87%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans=[]
+        arr1=[]
+        arr2=[]
+        for num in nums:
+            ans.append(num)
+        for num in nums:
+            ans.append(num)
+        return ans
+        
